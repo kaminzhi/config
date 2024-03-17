07f5485e1504ab355847472cd35f13e783092841
kaminzhi <issac20050710@gmail.com>
2024-03-18 04:18:40 +0800

    feat: 加入相關設定檔
    add neovim,kitty,tmux,fish config file

---
+++ fish/config.fish
@@ -0,0 +1,66 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    neofetch
+end
+
+set -x username 'jimmys@bigbyte.com.tw'
+set -x password 'bb@Lab-Pan'
+set -x base_url 'https://bigbyte.beta.instructure.com'
+set -x token '9898~vdaEBTjA8ZT4PIm36KJDt1v375R4MxLtQ2opQwtJrYSZixX2lEz62aKsFlIljGNN'
+
+set fish_greeting "Hello, $USER!"
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias vim nvim
+alias ls "colorls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+    status --is-command-substitution; and return
+
+    if test -f .nvmrc; and test -r .nvmrc
+        nvm use
+    else
+    end
+end
+
+switch (uname)
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+    source $LOCAL_CONFIG
+end
